1a4b87164c778e13cab4835c51103579a6180f8c
Arefin <ahmedarefinsajjad@gmail.com>
2023-04-18 23:12:51 +0600

    test3

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -41,7 +41,7 @@ describe('renders the app', () => {
   });
 
   it('should render the title', async () => {
-    expect(document.title).toBe("Ahmed Arefin");
+    expect(document.title).toBe('Ahmed Arefin');
   });
 
   it('can navigate to /about', async () => {
